43a00b4113ed1513bf39530144560db025e628b4
Amir-LA <amirmadman@gmail.com>
2021-04-13 16:07:49 +1000

    changed

--- Phonebook/Form1.cs
+++ Phonebook/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Phonebook
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd1 = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\E\Git\P2_ Phonebook\Phonebook\Phonebook\Telbook.mdf;Integrated Security=True";
+                conn.Open();
         }
     }
 }
